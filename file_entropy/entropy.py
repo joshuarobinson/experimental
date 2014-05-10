0ce7c56e1cc04a52f60f84a151149b59633d72f8
Joshua Robinson <joshua@purestorage.com>
2014-05-10 13:14:46 -0700

    Python version of file-entropy.

--- file_entropy/entropy.py
+++ file_entropy/entropy.py
@@ -1,24 +1,40 @@
+"""This script calculates the byte-wise entropy of all files listed on the
+    command-line. """
+
+#
+# Run as follows:
+# > python3 entropy.py *
+
 import math
 from collections import Counter
+import sys
 
-def GetByteEntropy(filename):
-    with open(filename) as f:
-        contents = f.read()
-        byte_counters = Counter(contents)
+def get_byte_entropy(filename):
+    try:
+        with open(filename, "rb") as fil:
+            contents = fil.read()
+            # Create histogram of each byte value.
+            byte_counters = Counter(contents)
 
-        h = 0.0
-        for b in byte_counters:
-            c = byte_counters[b]
-            p_i = c / len(contents)
-            h -= p_i * math.log(p_i, 2)
+            # Calculate entropy.
+            entropy = 0.0
+            for byte in byte_counters:
+                p_i = byte_counters[byte] / len(contents)
+                entropy -= p_i * math.log(p_i, 2)
 
-        return h
+            return round(entropy, 5)  # Round to five decimal places.
 
-    return None
+    except EnvironmentError:  # If file didn't open or doesn't exist.
+        return None
+
 
 def main():
-    f = "Makefile"
-    print("{}\t{}".format(f, GetByteEntropy(f)))
+    # Loop over each argv (except the first, which is the program's name).
+    for filename in sys.argv[1:]:
+        entropy = get_byte_entropy(filename)
+        if entropy:
+            print("{}\t{}".format(filename, entropy))
+
 
 if __name__ == '__main__':
     main()
