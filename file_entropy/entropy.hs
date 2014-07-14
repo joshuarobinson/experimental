faf9e9322af565eb9f74edbe638712afc4fe7bfa
Joshua Robinson <joshua@purestorage.com>
2014-07-13 19:01:32 -0700

    Partial implementation of haskell file_entropy.
    Missing the ability to specify files on the command line.
    
    And probably not very good haskell.

---
+++ file_entropy/entropy.hs
@@ -0,0 +1,27 @@
+-- Haskell version of file_entropy.
+--
+-- run via "runhaskell entropy.hs"
+
+import qualified Data.ByteString as BS
+import Data.List
+import Data.Word
+
+-- Return a histogram as a list of bucket counts.
+-- Note that we are ignoring the bucket values.
+histogram :: BS.ByteString -> [Int]
+histogram xs = [ BS.length l | l <- BS.group (BS.sort xs) ]
+
+entropyHelper :: Int -> Double -> Int -> Double
+entropyHelper total_count prev count = prev - (p_i * logBase 2.0 p_i)
+    where p_i = (fromIntegral count) / (fromIntegral total_count)
+
+calcEntropy :: [Int] -> Double
+calcEntropy [] = 0.0
+calcEntropy xs = foldl (entropyHelper total_count) 0.0 xs
+    where total_count = sum xs
+
+main = do
+	d <- BS.readFile "Makefile"
+        let counts = histogram d
+        let entropy = calcEntropy counts
+	putStrLn (show entropy)
